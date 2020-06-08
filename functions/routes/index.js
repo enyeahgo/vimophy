dd187a857dc6fc3ec73b223ed6fcea6e9bf75a3c
Inigo Orosco II <enyeahgo@gmail.com>
2020-06-08 14:03:47 +0800

    Third commit

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Vimophy' });
+});
+
+router.get('/p', function(req, res, next) {
+  res.render('section', {
+    title: 'Vimophy',
+    pageTitle: 'Portfolio'
+  });
 });
 
 module.exports = router;
